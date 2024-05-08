ede1dbf422285aea91ef8cfcbc009fcb990ec8bb
Nail <nash34@gmail.com>
2024-05-09 04:45:12 +0500

    ...

---
+++ localback/localback.sh
@@ -0,0 +1,60 @@
+nano ~/kirsa_nginxsudo apt install mysql-server nginx -y
+
+sudo mkdir /var/www/kirsa-front
+sudo chown pos:pos /var/www/kirsa-front
+
+# rsync -rav -e ssh --exclude='categories_tree.json' /home/nash/PycharmProjects/kirsa-front/dist/ sunrise:/var/www/kirsa-front
+# scp kirsa_nginx mikh:~/
+
+nano ~/kirsa_nginx
+sudo mv ~/kirsa_nginx /etc/nginx/sites-available/kirsa
+
+sudo ln -s /etc/nginx/sites-available/kirsa /etc/nginx/sites-enabled/kirsa
+sudo nginx -s reload
+
+###
+CREATE DATABASE demands27 CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'demands27'@'localhost' IDENTIFIED BY 'sXdfsdf33458Wwe1';
+GRANT ALL PRIVILEGES ON demands27.* TO 'demands27'@'localhost' WITH GRANT OPTION;
+flush privileges;
+###
+
+sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
+
+###
+skip-log-bin
+server-id               = 49
+max_binlog_size   = 100M
+relay-log = /var/lib/mysql/mysql-relay-bin
+relay-log-index = /var/lib/mysql/mysql-relay-bin.index
+replicate-do-db = demands27
+replicate-ignore-table  = demands27.loutgo
+replicate-ignore-table  = demands27.loutgopos
+###
+sudo service mysql restart
+
+scp demands27.dmp.gz sunrise:~/
+gunzip demands27.dmp.gz
+mysql -u demands27 -p demands27 < demands27.dmp
+
+CHANGE MASTER TO MASTER_HOST='10.252.1.2', MASTER_USER='replication', MASTER_PASSWORD='Rehima123', MASTER_LOG_FILE = 'binlog.000259', MASTER_LOG_POS=90338;
+start slave;
+show slave status;
+
+
+
+### django
+git clone git@github.com:tfyr/kirsa.git
+cd kirsa
+python3 -m venv env
+source env/bin/activate
+sudo apt install -y python3-dev gcc pkg-config libmysqlclient-dev
+pip3 install mysqlclient
+
+pip install -r requirements.txt
+
+scp telegram-0.1.6.tar.gz sunrise:~/
+scp promocodes-0.1.1.tar.gz sunrise:~/
+
+pip install ~/telegram-0.1.6.tar.gz 
+pip install ~/promocodes-0.1.1.tar.gz
