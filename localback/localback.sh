4bb887892348fb6e137d4725697692b1d35d26c8
Nail <nash34@gmail.com>
2024-07-09 22:19:11 +0500

    xxx

--- localback/localback.sh
+++ localback/localback.sh
@@ -1,26 +1,21 @@
-nano ~/kirsa_nginx
-
-sudo apt install mysql-server nginx redis -y
+sudo apt install mysql-server nginx redis python3-dev gcc pkg-config libmysqlclient-dev -y
 
 sudo mkdir /var/www/kirsa-front
 sudo chown pos:pos /var/www/kirsa-front
 sudo usermod -a -G pos www-data
 
+chmod 600 ~/.ssh/authorized_keys
+ssh-keygen
 
+cat ~/.ssh/id_rsa.pub
 # rsync -rav -e ssh --exclude='categories_tree.json' /home/nash/PycharmProjects/kirsa-front/dist/ sunrise:/var/www/kirsa-front
-scp daphne_kirsa.service kirsa_nginx requirements.txt telegram-0.1.6.tar.gz promocodes-0.1.1.tar.gz mikh:~/
 
-nano ~/kirsa_nginx
-sudo mv ~/kirsa_nginx /etc/nginx/sites-available/kirsa
+sudo mv ~/ubuntu_init/localback/kirsa_nginx /etc/nginx/sites-available/kirsa
+sudo nano /etc/nginx/sites-available/kirsa
 sudo ln -s /etc/nginx/sites-available/kirsa /etc/nginx/sites-enabled/kirsa
 sudo nginx -s reload
 
-###
-CREATE DATABASE demands27 CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'demands27'@'localhost' IDENTIFIED BY 'sXdfsdf33458Wwe1';
-GRANT ALL PRIVILEGES ON demands27.* TO 'demands27'@'localhost' WITH GRANT OPTION;
-flush privileges;
-###
+sudo mysql -e "CREATE DATABASE demands27 CHARACTER SET utf8 COLLATE utf8_general_ci;CREATE USER 'demands27'@'localhost' IDENTIFIED BY 'sXdfsdf33458Wwe1';GRANT ALL PRIVILEGES ON demands27.* TO 'demands27'@'localhost' WITH GRANT OPTION;flush privileges;"
 
 sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
 
@@ -33,19 +28,23 @@ relay-log-index = /var/lib/mysql/mysql-relay-bin.index
 replicate-do-db = demands27
 replicate-ignore-table  = demands27.loutgo
 replicate-ignore-table  = demands27.loutgopos
+replicate-ignore-table  = demands27.lincome
+replicate-ignore-table  = demands27.lincomepos
 replicate-ignore-table  = demands27.lbalance
 slave-skip-errors = 1062,1032
 ###
 sudo service mysql restart
 
-scp demands27.dmp.gz sunrise:~/
+# scp demands27.dmp.gz sunrise:~/
 gunzip demands27.dmp.gz
 mysql -u demands27 -p demands27 < demands27.dmp
 
+# ssh tupak 'echo "000000"| sudo -S mysql demands27 -e "ALTER TABLE demands27.loutgopos ADD column amc_code varchar(150) null;"'
+# ssh tupak 'echo "000000"| sudo -S mysql demands27 -e "ALTER TABLE demands27.loutgopos ADD CONSTRAINT loutgopos_amc_code_3d2375e0_uniq UNIQUE KEY (amc_code);"'
 # -------------------------------
 ALTER TABLE demands27.loutgopos DROP FOREIGN KEY loutgopos_outgo_id_6e59c97d_fk_loutgo_id;
 ALTER TABLE demands27.loutgopos ADD CONSTRAINT loutgopos_outgo_id_6e59c97d_fk_loutgo_id FOREIGN KEY (outgo_id) REFERENCES demands27.loutgo(id) ON DELETE CASCADE ON UPDATE RESTRICT;
-CHANGE MASTER TO MASTER_HOST='10.252.1.2', MASTER_USER='replication', MASTER_PASSWORD='Rehima123', MASTER_LOG_FILE = 'binlog.000259', MASTER_LOG_POS=90338;
+CHANGE MASTER TO MASTER_HOST='10.252.1.2', MASTER_USER='replication', MASTER_PASSWORD='Rehima123', MASTER_LOG_FILE = 'binlog.000259', MASTER_LOG_POS=90338 GET_MASTER_PUBLIC_KEY=1;
 start slave;
 show slave status \G;
 # ------------------------------
@@ -62,7 +61,6 @@ mkdir egais_cheques
 python3 -m venv env
 source env/bin/activate
 
-sudo apt install -y python3-dev gcc pkg-config libmysqlclient-dev
 pip3 install mysqlclient
 pip install -r ~/requirements.txt
 pip install ~/telegram-0.1.6.tar.gz ~/promocodes-0.1.1.tar.gz
--- wireguard.sh
+++ wireguard.sh
@@ -7,3 +7,5 @@ nano wg0.conf
 systemctl enable wg-quick@wg0
 wg-quick up wg0
 ping 10.252.1.1
+
+h9x4B0CuFR7wmcWg8oxnD4rXx2TKjpDI9T8PYJng
