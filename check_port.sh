c02da0fc0ec4b4ee32cddd563c2dc0be0cba7b96
Nail <nash34@gmail.com>
2025-01-26 02:36:49 +0500

    comproxy check ports

---
+++ check_port.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for f in /dev/ttyA* ;do
+    #udevadm info -q all -a $f | grep 'ATTRS{idProduct}=="903b"'   # Android terminal
+    #udevadm info -q all -a $f | grep 'ATTRS{manufacturer}=="NXP"'  # Shtrikh 
+    udevadm info -q all -a $f | grep 'ATTRS{product}=="STM32 Virtual ComPort  in FS Mode"'  # VikiPrint
+    if [ $? -eq 0 ]; then
+        f_escaped=$(echo $f | sed 's/\//\\\//g')
+        sed -i -e  "s/physical_port=\/dev\/ttyACM[0-9]/physical_port=$f_escaped/g" /opt/comproxy/ComProxy.ini
+    fi
+done
