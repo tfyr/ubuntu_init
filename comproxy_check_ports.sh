c07c25bdf466d8e9fdac90ffad94f30f7047dc66
pos <nash34@gmail.com>
2025-03-04 14:29:47 +0500

    remove sudo

--- comproxy_check_ports.sh
+++ comproxy_check_ports.sh
@@ -6,6 +6,6 @@ for f in /dev/ttyA* ;do
     udevadm info -q all -a $f | grep 'ATTRS{product}=="STM32 Virtual ComPort  in FS Mode"'   # Viki
     if [ $? -eq 0 ]; then
         f_escaped=$(echo $f | sed 's/\//\\\//g')
-        sudo sed -i -e  "s/physical_port=\/dev\/ttyACM[0-9]/physical_port=$f_escaped/g" /opt/comproxy/ComProxy.ini
+        sed -i -e  "s/physical_port=\/dev\/ttyACM[0-9]/physical_port=$f_escaped/g" /opt/comproxy/ComProxy.ini
     fi
 done
