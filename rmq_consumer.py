2c01534ac38d00bb7d280ad3fc7abb890a5ef77b
Nail <nash34@gmail.com>
2025-08-19 19:17:12 +0500

    rmq_consumer

---
+++ rmq_consumer.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from pika import BlockingConnection, ConnectionParameters
+
+telegram_bot_url1 = "https://api.telegram.org/bot1228961072:AAHRkM0SwgQdQVilZauHt1BreAaksQI3fpc/"
+
+connection = BlockingConnection(ConnectionParameters('localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='tg')
+
+def send_mess(chat, text, newurl=None, reply_markup=None, parse_mode=None):
+    params = {'chat_id': chat, 'text': text}
+    if reply_markup:
+        params['reply_markup'] = reply_markup
+    if parse_mode:
+        params['parse_mode'] = parse_mode
+    response = requests.post((url if not newurl else newurl)+ 'sendMessage', data=params)
+    return response
+
+def callback_tg(ch, method, properties, body):
+    print(f"Сообщение: {body.decode()}")
+
+    data = json.loads(body)
+    send_mess(
+        chat=data['chat_id'],
+        text=data['msg'],
+        newurl=telegram_bot_url1,)
+
+
+channel.basic_consume(queue='tg',
+                      on_message_callback=callback_tg,
+                      auto_ack=True)
+print("Ожидание сообщений")
+channel.start_consuming()
+
