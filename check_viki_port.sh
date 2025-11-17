03bf0f2b25aaa499b9ce6d214ada28e3c258e854
Nail <nash34@gmail.com>
2025-11-17 18:48:58 +0500

    ...

--- check_viki_port.sh
+++ check_viki_port.sh
@@ -7,5 +7,11 @@ for f in /dev/ttyA* ;do
     if [ $? -eq 0 ]; then
         f_escaped=$(echo $f | sed 's/\//\\\//g')
         sed -i -e  "s/physical_port=\/dev\/ttyACM[0-9]\+/physical_port=$f_escaped/g" /opt/comproxy/ComProxy.ini
+        systemctl status comproxy | grep 'active (running)'
+        if [ $? -eq 0 ]; then
+            echo $(date) >> /home/nash/restart_comproxy.txt
+            systemctl restart comproxy
+        fi
+        break
     fi
 done
