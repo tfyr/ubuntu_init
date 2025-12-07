9a6d3e0a19a02045f05b5aa2a19a417825c4d1ff
Nail <nash34@gmail.com>
2025-12-08 03:00:44 +0500

    udp forwarding for shtrikh weightscale

---
+++ udp_forwarding.sh
@@ -0,0 +1,15 @@
+sudo tee /etc/systemd/system/udp-forward.service << EOF
+[Unit]
+Description=UDP Port Forward 1111
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/socat UDP4-LISTEN:1111,fork,reuseaddr UDP4:192.168.1.202:1111
+Restart=always
+User=nobody
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable --now udp-forward.service
