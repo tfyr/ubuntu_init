5b3bf14b744436c1c9aaea055a691d8634b9ecac
Nail <nash34@gmail.com>
2026-07-04 18:38:11 +0500

    changes

--- server_init.sh
+++ server_init.sh
@@ -14,7 +14,7 @@ sudo systemctl disable comproxy
 
 wget http://ftp.us.debian.org/debian/pool/main/p/pangox-compat/libpangox-1.0-0_0.0.2-5.1_amd64.deb && sudo apt install ./libpangox-1.0-0_0.0.2-5.1_amd64.deb
 #wget --no-check-certificate https://egais.ru/files/distr/u-trans-4.2.0-2660-i386.deb
-wget http://27.9733.ru/u-trans-4.2.0-2660-i386.deb
+wget http://27.9733.ru/u-trans-4.2.0-2689-i386.deb
 wget http://27.9733.ru/Fito
 wget http://27.9733.ru/ShtrihPrintSetupRus.exe 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -31,7 +31,6 @@ sudo apt install -y openssh-server curl socat libgtk2.0-0:i386 libc6:i386 libstd
 mkdir -p .ssh && printf "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD2nNZJpGtQDYQasPS3Om+lBQlvC0QPhnXCxiiC7nbmHnHhhi4gHlmKGI6tzrmz5VHMHZzq12wuCGQrvOHLAsXkBpcQ+vtmKjEaMzxG4KYueqsISMNcpHQIJnP8GSZySGreflfJWHwIusK/MV9BngX0f9eEeei8p3N0vOCOWHY17dYo0bEBwXCX/Tg7ghhcmsiNl6YXeFGE/wV5hT2edhgf+d3mExCBdxPHdy+hpbvzGULgzMW/BeryzFS3grA5lXmqDLBBu+i9f6QXi8s23+UTkafZPdo3B+B1h18TxQFGot4Ck/oUjCjpMUtCbKSyIvhAElGU4fkTOs6nXN4wwVwJ nash@nashlinux
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7dlcxl9dJwy5uoVi/vatHfO6HprFkGJsP6SS63WJmMeRvOGKYOG/QijbUDEwaYVJhyBo5VZdW7XEFJz+xlR3wBtP6aY1pSZ0iSBN/ej3xo7ifZxPPN32I+Nam7Fc5ez2031QaRVo9Dft8szKzX+wgjQ1tvY2roPIgN404TJlgAJDS9z6JDDlKqK2TdUoYTpFAMU1gWtdAiIQvdh5/pCii75Ttl82UwjFgXp98+jDtn2eAunb2YQCSzQkUPMnC5Y+l4T3funhWto4fFahmL+wE9gEkAiVf59I91v3gDJJwzPQoUgGeFL17ruPkD5r6neUZ40OWSno9+zcGMjIDFTSB1ztl8vjO9eVeRVhOcN2Z1RY7C/2NgvplyM4sAry7UkcsBNKjA5/giMqpMbRTQr31tti0Zd9OWrbSPPaQH/f6Nq3uIhASRs+dsRZ5lyWzFkqt1m8DDrAHsULJTI0gdXRc+c4P0m2K7lnSiT34hEIBo/YtoTYRc1rTbrhaef7lhi8= mac@MacBook-Pro-Mac.local
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZiPVOgigcx060jfF0poy9WHnCakea8Muq6k67fmD7W1gxsSF7WzpjCSMkLNyWiFraf46ia/XFy6IneaUh5a1Sg2GBYtu6UfZ0UjsSZd2NOY7fUKshJYen5DeamnlrVfSY1oNDnNRKFxjiG3HblQVC4Jdb3NbMsYvzYCyvktTYvk7MGF6uOVSeX5dYDCrQwU9rXEIfjwIora8NBjqGx/mAZA1GQPavvvCNQSGA97c/nYEvtbNUjzeQIVPpa3gifoErVJ4sjnKub4FEjzAIdVEo683wL3SH79n9dc8UTXcAIS5U6D0PpMjRvAOYTmQi2+U11ff667uzelPR/wMBdMMJqj4/HwcfpBAO0pJsezQmVXnqWzQPtKvujpa2nfoNwYUxNSSTJwG2dNcOZJNu3dBuYlIj1zqGytA0okPfG8CHQVYv8H9EmEQfq8Mf4T51PNP5Tbr7xbC43jmz3rV9GjLJFZszmV7x1KLbSE81mwfSgRnJAzbWO1YaD/fOuQMmg3U= nash@v2164111.hosted-by-vdsina.ru
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7dlcxl9dJwy5uoVi/vatHfO6HprFkGJsP6SS63WJmMeRvOGKYOG/QijbUDEwaYVJhyBo5VZdW7XEFJz+xlR3wBtP6aY1pSZ0iSBN/ej3xo7ifZxPPN32I+Nam7Fc5ez2031QaRVo9Dft8szKzX+wgjQ1tvY2roPIgN404TJlgAJDS9z6JDDlKqK2TdUoYTpFAMU1gWtdAiIQvdh5/pCii75Ttl82UwjFgXp98+jDtn2eAunb2YQCSzQkUPMnC5Y+l4T3funhWto4fFahmL+wE9gEkAiVf59I91v3gDJJwzPQoUgGeFL17ruPkD5r6neUZ40OWSno9+zcGMjIDFTSB1ztl8vjO9eVeRVhOcN2Z1RY7C/2NgvplyM4sAry7UkcsBNKjA5/giMqpMbRTQr31tti0Zd9OWrbSPPaQH/f6Nq3uIhASRs+dsRZ5lyWzFkqt1m8DDrAHsULJTI0gdXRc+c4P0m2K7lnSiT34hEIBo/YtoTYRc1rTbrhaef7lhi8= mac@MacBook-Pro-Mac.local
 " >> ~/.ssh/authorized_keys
 printf "PermitRootLogin no
 PasswordAuthentication no
@@ -101,4 +100,4 @@ sed -i 's|/usr/bin/google-chrome-stable|/usr/bin/google-chrome-stable --remote-d
 update-desktop-database ~/.local/share/applications/
 
 #pkcs11-tool --module $(find  /usr/*(lib|lib64) -name librtpkcs11ecp.so) -O
-#pkcs11-tool --module $(find  /usr/*(lib|lib64) -name librtpkcs11ecp.so) -r -y cert --id 34414438464430334338464234414533 | openssl x509 -inform der -text
+#pkcs11-tool --module $(find  /usr/*(lib|lib64) -name librtpkcs11ecp.so) -r -y cert --id 34414438464430334338464234414533 | openssl x509 -inform der -text -nameopt utf8,sep_multiline
