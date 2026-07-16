adee1261502ef3106c373acbcf83a127b9861242
Nail Sharipov <nash34@gmail.com>
2026-07-17 04:35:17 +0500

    rabbit connection

--- tg_to_test_channel.py
+++ tg_to_test_channel.py
@@ -1,11 +1,14 @@
 import sys
 from pika import BlockingConnection, ConnectionParameters
+from pika.credentials import PlainCredentials
+from secret import rabbit_remote_user, rabbit_remote_password, rabbit_remote_host
+
 
 
 test_chat_id = -534237299
 exchange_errors_chat_id = -4621737762
 
-rabbit_connection = BlockingConnection(ConnectionParameters('localhost'))
+rabbit_connection = BlockingConnection(ConnectionParameters(host=rabbit_remote_host, credentials=PlainCredentials(rabbit_remote_user, rabbit_remote_password) ))
 channel = rabbit_connection.channel()
 channel.queue_declare(queue='tg')
 
