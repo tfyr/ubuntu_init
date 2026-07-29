e0288eeddfade158b5307d9a2a107a8fd556bb4e
Nail <nash34@gmail.com>
2026-07-29 14:31:03 +0500

    comproxy works scripts

--- server_init.sh
+++ server_init.sh
@@ -12,6 +12,10 @@ chmod +x install_comproxy_2.4.26_409.sh
 sudo bash install_comproxy_2.4.26_409.sh
 sudo systemctl disable comproxy
 
+
+запустить задачи из comproxy_works
+
+
 wget http://ftp.us.debian.org/debian/pool/main/p/pangox-compat/libpangox-1.0-0_0.0.2-5.1_amd64.deb && sudo apt install ./libpangox-1.0-0_0.0.2-5.1_amd64.deb
 #wget --no-check-certificate https://egais.ru/files/distr/u-trans-4.2.0-2660-i386.deb
 wget http://27.9733.ru/u-trans-4.2.0-2689-i386.deb
@@ -32,6 +36,7 @@ mkdir -p .ssh && printf "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD2nNZJpGtQDYQasPS
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7dlcxl9dJwy5uoVi/vatHfO6HprFkGJsP6SS63WJmMeRvOGKYOG/QijbUDEwaYVJhyBo5VZdW7XEFJz+xlR3wBtP6aY1pSZ0iSBN/ej3xo7ifZxPPN32I+Nam7Fc5ez2031QaRVo9Dft8szKzX+wgjQ1tvY2roPIgN404TJlgAJDS9z6JDDlKqK2TdUoYTpFAMU1gWtdAiIQvdh5/pCii75Ttl82UwjFgXp98+jDtn2eAunb2YQCSzQkUPMnC5Y+l4T3funhWto4fFahmL+wE9gEkAiVf59I91v3gDJJwzPQoUgGeFL17ruPkD5r6neUZ40OWSno9+zcGMjIDFTSB1ztl8vjO9eVeRVhOcN2Z1RY7C/2NgvplyM4sAry7UkcsBNKjA5/giMqpMbRTQr31tti0Zd9OWrbSPPaQH/f6Nq3uIhASRs+dsRZ5lyWzFkqt1m8DDrAHsULJTI0gdXRc+c4P0m2K7lnSiT34hEIBo/YtoTYRc1rTbrhaef7lhi8= mac@MacBook-Pro-Mac.local
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZiPVOgigcx060jfF0poy9WHnCakea8Muq6k67fmD7W1gxsSF7WzpjCSMkLNyWiFraf46ia/XFy6IneaUh5a1Sg2GBYtu6UfZ0UjsSZd2NOY7fUKshJYen5DeamnlrVfSY1oNDnNRKFxjiG3HblQVC4Jdb3NbMsYvzYCyvktTYvk7MGF6uOVSeX5dYDCrQwU9rXEIfjwIora8NBjqGx/mAZA1GQPavvvCNQSGA97c/nYEvtbNUjzeQIVPpa3gifoErVJ4sjnKub4FEjzAIdVEo683wL3SH79n9dc8UTXcAIS5U6D0PpMjRvAOYTmQi2+U11ff667uzelPR/wMBdMMJqj4/HwcfpBAO0pJsezQmVXnqWzQPtKvujpa2nfoNwYUxNSSTJwG2dNcOZJNu3dBuYlIj1zqGytA0okPfG8CHQVYv8H9EmEQfq8Mf4T51PNP5Tbr7xbC43jmz3rV9GjLJFZszmV7x1KLbSE81mwfSgRnJAzbWO1YaD/fOuQMmg3U= nash@v2164111.hosted-by-vdsina.ru
 " >> ~/.ssh/authorized_keys
+
 printf "PermitRootLogin no
 PasswordAuthentication no
 ChallengeResponseAuthentication no
